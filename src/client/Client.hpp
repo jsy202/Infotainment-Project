cd59b50daa0e7dfa6e7e07d3f9d6d9a67f4f6161
jsy202 <oie22230@gmail.com>
2025-03-05 19:31:55 +0900

    Update Client.hpp

--- src/client/Client.hpp
+++ src/client/Client.hpp
@@ -1,1 +1,27 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <vsomeip/vsomeip.hpp>
+#include <QObject>
+
+class Client : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(int speedData READ speedData NOTIFY speedDataChanged)
+
+public:
+    Client();
+    void start();
+    int speedData() const { return speedData_; }
+
+signals:
+    void speedDataChanged(int speed);
+
+private:
+    void onDataReceived(const std::shared_ptr<vsomeip::message> &msg);
+
+    std::shared_ptr<vsomeip::application> app_;
+    int speedData_;
+};
+
+#endif // CLIENT_HPP
 
