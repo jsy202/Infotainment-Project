99a2c7ebdb2ee52f3571e7d9ed7864a00001798f
jsy202 <oie22230@gmail.com>
2025-03-05 19:24:11 +0900

    Update Constants.hpp

--- src/common/Constants.hpp
+++ src/common/Constants.hpp
@@ -1,1 +1,14 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+namespace Constants {
+    constexpr vsomeip::service_t SERVICE_ID = 0x1234;
+    constexpr vsomeip::instance_t INSTANCE_ID = 0x01;
+    constexpr vsomeip::method_t METHOD_ID = 0x5678;
+
+    constexpr const char *SERVER_APP_NAME = "DataServer";
+    constexpr const char *CLIENT_APP_NAME = "DataClient";
+}
+
+#endif // CONSTANTS_HPP
 
