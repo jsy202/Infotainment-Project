545c2ec1c2f400baf4b6660c0d452cf57989d98a
jsy202 <oie22230@gmail.com>
2025-03-05 19:24:55 +0900

    Update Server.hpp

--- src/server/Server.hpp
+++ src/server/Server.hpp
@@ -1,1 +1,20 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <vsomeip/vsomeip.hpp>
+#include <thread>
+#include <memory>
+
+class Server {
+public:
+    Server();
+    void start();
+
+private:
+    void sendData();
+    std::shared_ptr<vsomeip::application> app_;
+    std::thread senderThread_;
+};
+
+#endif // SERVER_HPP
 
