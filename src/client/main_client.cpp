25ea8e01f0c40a8d880cb3de22fcd8c23c55d733
jsy202 <oie22230@gmail.com>
2025-03-05 19:32:42 +0900

    Update main_client.cpp

--- src/client/main_client.cpp
+++ src/client/main_client.cpp
@@ -1,1 +1,18 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "Client.hpp"
+
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+
+    Client client;
+    client.start();
+
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("dataClient", &client);
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    return app.exec();
+}
 
